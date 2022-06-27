f37e636026e859389c2261c0faa45cf6defbf629
hs7 <sato.hikaru.dev@gmail.com>
2022-06-27 20:05:35 +0900

    add UserFavoriteLocalDataSource

---
+++ GitHub/GitHub/Class/Logic/DataLayer/LocalDataSource/UserFavoriteLocalDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  UserFavoriteLocalDataSource.swift
+//  GitHub
+//
+//  Created by Hikaru Sato on 2022/06/27.
+//
+
+import Foundation
+
+
+protocol UserFavoriteLocalDataSource {
+
+}
+
+struct UserFavoriteLocalDataSourceImpl: UserFavoriteLocalDataSource {
+
+}
+
